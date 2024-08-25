9dcda0d16181dcd2f8f0dd3927a215e294f90837
warutr <warutruangvatanasirikul@gmail.com>
2024-08-25 09:52:54 +0700

    add comment folder and create rate.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-const homePage = () => {
+const mainPage = () => {
   return (
     <div className="flex flex-col items-center gap-9 w-[330px] my-16 xl:w-[1100px] xl:gap-16">
-      <div className="flex flex-col gap-9 xl:flex-row">homePage</div>
+      <div className="flex flex-col gap-9 xl:flex-row">
+        <div className="flex flex-col gap-6 text-center xl:text-start xl:gap-5 xl:w-1/2">
+          <h1 className="text-Very-Dark-Magenta font-bold text-[40px] leading-8 px-2 xl:pr-44 xl:px-0">
+            10,000+ of our users love our products.
+          </h1>
+          <p className="text-Dark-Grayish-Magenta px-[18px] xl:px-0 xl:pr-36">
+            We only provide great products combined with excellent customer
+            service. See what our satisfied customers are saying about our
+            services.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default homePage;
+export default mainPage;
 
 /* 
 
